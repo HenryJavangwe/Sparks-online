a24eb5146e3c449f473e67ab339d1c77fbfd28fb
Henry Javangwe <henrysparks1@gmail.com>
2020-06-07 22:15:33 +0200

    renamed the route Home to home

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const logger = require('morgan');
 // Link Routes
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
-const homeRouter = require('./routes/Home');
+const homeRouter = require('./routes/home');
 
 const app = express();
 
@@ -23,7 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/Home', homeRouter);
+app.use('/home', homeRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/home.js
+++ routes/home.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res)=>{
-    res.render('Home')
+    res.render('home')
 })
 
 module.exports = router;
