6398e17955da77b8e20195cd2d60b689d69b4f79
Henry Javangwe <henrysparks1@gmail.com>
2020-06-08 10:12:49 +0200

    changed the arrow function to a regular name function.

--- routes/home.js
+++ routes/home.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res)=>{
-    res.render('home')
+router.get('/', function (req, res){
+    res.render('home');
 })
 
 module.exports = router;
